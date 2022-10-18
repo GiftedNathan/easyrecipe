e45e579809c7920233c732d48e7071da3224cbe2
giftedNathan <eje.nathaniel@gmail.com>
2022-10-18 12:07:22 +0100

    Finished the header and hero sections

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,20 +1,32 @@
 import styled from "styled-components"
+import chefCap from "../assets/images/chef-cap.png"
 
 
 const Navbar = styled.div`
     width: 100%;
-    padding: 30px;
-    
+    /* background-color: #61a734; */
+    background-color: #e3effb;
+    border-bottom: 1px solid #fefefe;
+    /* padding: 30px; */
 
 `
 const Logo = styled.a`
-    font-size: 18px;
-    color: red;
+    display: flex;
+    align-items: center;
+    font-family: 'Inspiration', cursive;
+    font-size: 38px;
+    color: #61a734;
 `
+
+const LogoImage = styled.img`
+    width: 60px;
+
+    margin: 15px;
+`;
 const Header = () => {
     return (
         <Navbar>
-            <Logo>ease recipe</Logo>
+            <Logo> <LogoImage src={chefCap} alt="recipe-log" /> easy <span>Resipe</span></Logo>
         </Navbar>
     )
 } 
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,84 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import chefLady from '../assets/images/chef-man.png'
 
 
+// font-family: 'Fuzzy Bubbles', cursive;
+
+const HeroSection = styled.div`
+  width: 100%;
+  min-height: 100vh;
+
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+
+  padding: 30px 160px;
+  /* background-color: #61a734; */
+
+  background-color: #e3effb;
+
+  .left{
+    /* flex: 1; */
+    max-width: 340px;
+    border: 1px solid gray;
+    
+  }
+  .left h1{
+    width: fit-content;
+    font-family: 'Inspiration', cursive;
+    font-size: 62px;
+    text-transform: capitalize;
+    letter-spacing: 5px;
+    color: #ff5a00;
+    position: relative;
+  }
+  .left h1::after{
+    position: absolute;
+    content: 'fresh food';
+    top: 0px;
+    right: -50px;
+
+    font-family: poppins;
+    font-size: 18px;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    color: #61a734;
+    
+  }
+
+  .right{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* flex: 1; */
+    width: 50%;
+  }
+
+  .right img{
+    width: 400px;
+    border: 1px solid gray;
+  }
+
+  
+`
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <HeroSection>
+      <div className="left">
+        <h1>easy recipe</h1>
+        <p>
+          how your food is prepared have a major impact 
+          on your mental and emotional health as well as your growth.
+        </p>
+      </div>
+      <div className="right">
+        <img src={chefLady} alt="" />
+      </div>
+    </HeroSection>
   )
 }
 
