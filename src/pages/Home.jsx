fe3d9f4eb6d95c3e19a934b2c78e81752064c70d
giftedNathan <eje.nathaniel@gmail.com>
2022-10-24 12:12:58 +0100

    Added react-router-dom

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../sections/Header'
+import Hero from '../sections/Hero'
+import Features from '../sections/Features'
+
+const Home = () => {
+  return (
+    <div className="contianer">
+
+      <Header />
+      
+      <Hero />
+
+      <Features />
+
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Recipes.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../sections/Header'
+
+const Recipes = () => {
+
+  
+  return (
+    <div>
+      
+      <Header />
+      <h1>all recipes please</h1>
+    </div>
+  )
+}
+
+export default Recipes
