12bf4107ae822c776de0db802d1c34a77664ad63
giftedNathan <eje.nathaniel@gmail.com>
2022-10-24 14:40:52 +0100

    Added recipes page

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -7,13 +7,60 @@ const RecipesContainer = styled.div`
   background-color: goldenrod;
 `
 
+const RecipeHero = styled.section`
+  
+  width: 100%;
+  /* min-height: 100vh; */
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  padding: 60px 30px;
+
+  h1{
+    font-size: 2.5em;
+    text-align: center;
+    text-transform: capitalize;
+    margin-bottom: 20px;
+  }
+
+  p{
+    text-align: center;
+    margin-bottom: 40px;
+  }
+
+  form{
+    max-width: 560px;
+    min-width: 280px;
+  }
+  form input {
+    width: 100%;
+    height: 40px;
+    padding-left: 15px;
+
+    border: none;
+    outline: none;
+    border-radius: 20px;
+  }
+`
+
 const Recipes = () => {
 
 
   return (
     <RecipesContainer>
       <Header />
-      <h1>all recipes please</h1>
+      <RecipeHero>
+        <h1>search your favorite recipes</h1>
+        <p>Remember, it takes a good recipe to prapare appealing delicacies.</p>
+
+        <form action="" method="post">
+          <input type="search" name="search-recipe" id="" />
+        </form>
+      </RecipeHero>
+      
     </RecipesContainer>
   )
 }
